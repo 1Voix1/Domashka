9049fe1343cd03931e3efa450d71b7ac6d29ed5c
1Voix1 <azamat.zaripov.04@Gmail.com>
2025-10-03 12:51:49 +0500

    P-17 03.10 Сделано

---
+++ P-17/zadanie2.py
@@ -0,0 +1,6 @@
+x = int(input("ВВедите 1 число: "))
+y = int(input("ВВедите 2 число: "))
+T = y
+y = x
+x = T
+print(x, y)
---
+++ P-17/zadanie3.py
@@ -0,0 +1,7 @@
+import math
+
+R1 = 5.6
+R2 = 3.8
+S1 = R1**2 * math.pi
+S2 = R2**2 * math.pi
+print(f"Площадь кольца {S1 - S2}")
